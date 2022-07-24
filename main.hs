5aa9db1653442af50eef8b650e9c8224fc7f1069
gabor <gaborspaits1@gmail.com>
2022-07-24 18:34:48 +0200

    refactored version w tuples

--- main.hs
+++ main.hs
@@ -8,37 +8,33 @@ processFrames [] _ _ _ = 0
 processFrames (x:xs) spa str 0 = undefined
 processFrames (x:xs) spa str n = undefined
 
-processFrame :: ([Char], Int, Int) -> Bool -> (Int, Int, Int)
-processFrame f@([o], spa, str) bonus = processOneThrow f bonus
-processFrame fl@([f,s], spa, str) False = processTwoThrows fl
+processFrame :: ([Char], Int, Int, Int) -> (Int, Int, Int, Int)
+processFrame f@([o], spa, str, n) = processOneThrow f
+processFrame fl@([f,s], spa, str, n) = processTwoThrows fl
 --TODO
 
-processTwoThrows :: ([Char], Int, Int) -> (Int, Int, Int)
-processTwoThrows ([a, '/'], spa, str) 
- | isDigit a = (calculateTwoThrowsScore 10 (digitToInt a) spa str, 1, 0)
+processTwoThrows :: ([Char], Int, Int, Int) -> (Int, Int, Int, Int)
+processTwoThrows ([a, '/'], spa, str, n) 
+ | isDigit a = (calculateTwoThrowsScore 10 (digitToInt a) spa str, 1, 0, decraseUntilZero n)
  --TODO
-processTwoThrows ([a, b], spa, str)
- | isDigit a && isDigit b && sumThrow < 10 = (calculateTwoThrowsScore sumThrow firstThrow spa str,str,0)
+processTwoThrows ([a, b], spa, str, n)
+ | isDigit a && isDigit b && sumThrow < 10 = (calculateTwoThrowsScore sumThrow firstThrow spa str, str, 0, decraseUntilZero n)
  --TODO
  where
   sumThrow = firstThrow + digitToInt b 
   firstThrow = digitToInt a
 
-processOneThrow :: ([Char], Int, Int) -> Bool -> (Int, Int, Int)
-processOneThrow (['x'], spa, str) bonus = (calculateOneThrowScore 10 spa str bonus, str ,1)
-processOneThrow ([n], spa, str) True
- | isDigit n = (calculateOneThrowScore (digitToInt n) spa str True, str, 0)
+processOneThrow :: ([Char], Int, Int, Int) -> (Int, Int, Int, Int)
+processOneThrow (['x'], spa, str, n)  = (calculateOneThrowScore 10 spa str n, str , 1, decraseUntilZero n)
+processOneThrow ([o], spa, str, n) 
+ | isDigit o = (calculateOneThrowScore (digitToInt o) spa str n, str, 0, decraseUntilZero n)
  -- TODO
 
-calculateOneThrowScore :: Int -> Int -> Int -> Bool -> Int
-calculateOneThrowScore scr spa str bonus = ((boolToInt (not bonus)) + spa + str) * scr
+calculateOneThrowScore :: Int -> Int -> Int -> Int -> Int
+calculateOneThrowScore scr spa str n = ((signum n) + spa + str) * scr
 
 calculateTwoThrowsScore :: Int -> Int -> Int -> Int -> Int
 calculateTwoThrowsScore sum first spa str = (1 + str) * sum + first * spa
-
-boolToInt :: Bool -> Int
-boolToInt False = 0
-boolToInt _ = 1
 
 decraseUntilZero :: Int -> Int
 decraseUntilZero 0 = 0
